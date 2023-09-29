d0c29513103fe2cf1a64d31258c45880ef4075d2
HasanOmar1 <r3domar1@gmail.com>
2023-09-29 13:22:54 +0300

    added

---
+++ solution.js
@@ -0,0 +1,45 @@
+const carMarket =  require("./obj");
+
+// 1 Agency Operations
+
+// Search for a car agency by its name or ID.
+
+// const getAgencyByNameOrId = (name) => {
+//     return carMarket.sellers.find(
+//     (agency) => agency.agencyName === name || 
+//     agency.agencyId === name) 
+         
+//         }
+// console.log(getAgencyByNameOrId('Best Deal'))
+
+
+
+// Retrieve all agencies' names
+
+// let arr = []
+// carMarket.sellers.forEach( name => {
+
+//     arr.push( {agencyName : name.agencyName} )
+// })
+// console.log(arr)
+
+
+
+//Add a new car to an agency's inventory.
+
+        // carMarket.sellers[0].cars[0] = {
+        //     brand : 'Ferarri',
+        //     models : [
+        //         {
+        //             name : 'sonic',
+        //             year : 2023,
+        //             price : 500000,
+        //             carNumber : 'ASD9',
+        //             ownerId : "Plyq5M5AZ"
+        //         }
+        //     ]
+        // }
+        // console.log(carMarket.sellers[0])
+        // console.log(Object.values(carMarket.sellers[0].cars[0]))
+
+        
